906ec5081d673829451c430dc39b0b808044a303
SleepWalker <0yatsenko.r0@gmail.com>
2024-01-15 16:27:12 +0300

    2-product

--- src/route/index.js
+++ src/route/index.js
@@ -2,62 +2,78 @@
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
+// ================================================================
+
+// const obj = [
+// {
+//   name: 'Roman',
+//   lastname: 'Yatsenko',
+//   id: 323
+// },
+// {
+//   name: 'Kolya',
+//   lastname: 'Vantsev',
+//   id: 322
+// }];
+// console.log(obj)
+
+// updateById = (id) => {
+//   const productId = obj.findIndex(elem => elem.id === id)
+
+//   if (productId !== -1) {
+//     obj[productId].name = newname;
+//     obj[productId].lastname = newlastname;
+//     return obj;
+//   } else {
+//     return 'Error'
+//   }
+// }
+// let newname = 'Sergey'
+// let newlastname = 'Piatachenko'
+
+// console.log(updateById(322));
+
+
 
 // ================================================================
 
-class User {
+class Product {
+
   static #list = [];
 
-  constructor (email, login, password) {
-    this.email = email;
-    this.login = login;
-    this.password = password;
-    this.id = new Date().getTime()
-  };
-
-  verifyPassword = (password) => this.password === password;
-
-  static add = (user) => {
-    this.#list.push(user)
-  };
-
-  static getList = () =>  this.#list
-
-  static getById = (id) => this.#list.find((user) => user.id === id)
-
-  static deleteById = (id) => {
-    const index = this.#list.findIndex(
-      (user) => user.id === id
-    )
-
-    if (index !== -1) {
-      this.#list.splice(index, 1)
-      return true;
-    } else {
-      return false;
-    }
+  constructor (name, price, description) {
+    this.id = Math.floor(Math.random() * (99_000 - 10_000) + 10_000);
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.createDate = new Date().getTime();
   }
+
+  static getList = () => this.#list;
+
+  static add = (product) => this.#list.push(product);
+
+  static getById = (id) => this.#list.find( product => product.id === id);
 
   static updateById = (id, data) => {
-    const user = this.getById(id)
-    console.log(user)
+    const productIndex = this.#list.findIndex(elem => elem.id === id);
 
-    if (user) {
-      this.update(user, data)
-
-      return true
-    } else {
-      return false
-    }
+    if (productIndex !== -1) {
+      this.#list[productIndex].name = data.name;
+      this.#list[productIndex].price = data.price;
+      this.#list[productIndex].description = data.description;
+      return true;
+    } else {return false}
   }
 
-  static update = (user, {email}) => {
-    if (email) {
-      user.email = email;
-    }
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(elem => elem.id === id)
+
+    if (index !== -1) {
+      this.#list.splice(index, 1);
+      return true;
+    } else {return false}
   }
-
-
 }
 
 // ================================================================
@@ -68,7 +84,7 @@ class User {
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
-  const list = User.getList();
+  const list = Product.getList();
 
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
@@ -76,7 +92,7 @@ router.get('/', function (req, res) {
     style: 'index',
 
     data: {
-      users: {
+      products: {
         list,
         isEmpty: list.length === 0,
       }
@@ -88,51 +104,23 @@ router.get('/', function (req, res) {
 
 // ================================================================
 
-router.post('/success-info', function (req, res) {
-  const { email, login, password } = req.body;
-
-  const user = new User(email, login, password);
-
-  User.add(user)
-
-  console.log(User.getList())
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Пользователь создан!',
-  })
-})
-
-// ================================================================
-
-router.get('/success-info', function (req, res) {
-  const { id } = req.query;
-  
-  User.deleteById(Number(id))
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Пользователь удалён!',
-  })
-})
-
-// ================================================================
-
-router.post('/user-update', function (req, res) {
-  const { email, password, id } = req.body;
-
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body;
   let result = false;
 
-  const user = User.getById(Number(id))
+  const product = new Product (name, price, description);
 
-  if (user.verifyPassword(password)) {
-    User.updateById(Number(id), { email });
+  if (Object.keys(product).length !== 0) {
+    Product.add(product);
     result = true;
   }
 
-  res.render('success-info', {
-    style: 'success-info',
-    info: result ? 'Email почта обновлена!': 'Ошибка!',
+  console.log(Product.getList())
+
+  res.render('alert', {
+    style: 'alert',
+    info_title: result ? 'Успешное выполнение действия': 'Ошибка при выполнении действия',
+    info: result ? 'Товар был добавлен успешно': 'Товар не был добавлен',
   })
 })
 
