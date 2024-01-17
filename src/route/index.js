402040a2fe60c7c678112c7155af24e164ed59c0
SleepWalker <0yatsenko.r0@gmail.com>
2024-01-17 16:18:05 +0300

    2-product completed

--- src/route/index.js
+++ src/route/index.js
@@ -56,14 +56,22 @@ class Product {
   static getById = (id) => this.#list.find( product => product.id === id);
 
   static updateById = (id, data) => {
-    const productIndex = this.#list.findIndex(elem => elem.id === id);
+    const user = this.getById(id)
 
-    if (productIndex !== -1) {
-      this.#list[productIndex].name = data.name;
-      this.#list[productIndex].price = data.price;
-      this.#list[productIndex].description = data.description;
-      return true;
-    } else {return false}
+    if (user) {
+      this.update(user, data)
+
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (user, {name, price, description}) => {
+      user.name = name;
+      user.price = price;
+      user.description = description;
+      return user;
   }
 
   static deleteById = (id) => {
@@ -107,12 +115,14 @@ router.get('/', function (req, res) {
 router.post('/product-create', function (req, res) {
   const { name, price, description } = req.body;
   let result = false;
-
-  const product = new Product (name, price, description);
-
-  if (Object.keys(product).length !== 0) {
+  
+  if (name.trim() !== '' && price.trim() !== '') {
+    console.log('Запустился if')
+    const product = new Product (name, price, description);
     Product.add(product);
     result = true;
+  } else {
+    result = false;
   }
 
   console.log(Product.getList())
@@ -121,6 +131,97 @@ router.post('/product-create', function (req, res) {
     style: 'alert',
     info_title: result ? 'Успешное выполнение действия': 'Ошибка при выполнении действия',
     info: result ? 'Товар был добавлен успешно': 'Товар не был добавлен',
+  })
+})
+
+// ================================================================
+
+router.get('/product-create', function (req, res) {
+  const { name, price, description } = req.query;
+  
+
+  res.render('product-create', {
+    style: 'product-create',
+  })
+})
+
+// ================================================================
+
+// router.post('/alert', function (req, res) {
+//   const { name, price, description } = req.body;
+//   let result = false;
+
+//   const product = new Product (name, price, description);
+  
+//   if (Object.keys(product).length !== 0) {
+//     Product.add(product);
+//     result = true;
+//   }
+
+//   console.log(Product.getList())
+
+//   res.render('alert', {
+//     style: 'alert',
+//     info_title: result ? 'Успешное выполнение действия': 'Ошибка при выполнении действия',
+//     info: result ? 'Товар был добавлен успешно': 'Товар не был добавлен',
+//   })
+// })
+
+// ================================================================
+
+router.get('/product-list', function (req, res) {
+  const list = Product.getList();
+  
+
+  res.render('product-list', {
+    style: 'product-list',
+    list,
+  })
+})
+
+// ================================================================
+
+router.get('/product-edit', function (req, res) {
+  const { id } = req.query;
+  const product = Product.getById(Number(id))
+
+  res.render('product-edit', {
+    style: 'product-edit',
+    product,
+  })
+})
+
+// ================================================================
+
+router.post('/product-edit', function (req, res) {
+  const { name, price, description, id } = req.body;
+  let result = Boolean;
+  if ( name.trim() !== '' && price.trim() !== '') {
+  Product.updateById(Number(id), {name, price, description});
+  result = true
+  } else { result = false }
+  
+
+  res.render('alert', {
+    style: 'alert',
+    info_title: result ? 'Успешное выполнение действия': 'Ошибка при выполнении действия',
+    info: result? 'Данные успешно изменены': 'Ошибка при изменении данных',
+  })
+})
+
+// ================================================================
+
+router.get('/product-delete', function (req, res) {
+  const { id } = req.query;
+  let result = false;
+  if (Product.deleteById(Number(id))) {
+    result = true
+  }
+
+  res.render('alert', {
+    style: 'alert',
+    info_title: result ? 'Успешное выполнение действия': 'Ошибка при выполнении действия',
+    info: result? 'Данные успешно изменены': 'Ошибка при изменении данных',
   })
 })
 
